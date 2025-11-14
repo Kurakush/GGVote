84fdf3b4cec3b19de8bbac06b4132e87108da6c6
DEGRELLE Thomas <thomas.degrelle2@etu.univ-lorraine.fr>
2025-11-14 11:19:54 +0100

    Ajout d'éléments pour se connecter

---
+++ header.php
@@ -0,0 +1,45 @@
+<?php 
+require('dbconnect.php');
+
+//Load Session Variables
+session_start();
+
+//check disconnect
+if (isset($_GET["disconnect"])){
+    if ($_GET["disconnect"]==1){
+        unset($_SESSION["login"]);
+    }
+}
+
+//check credentiels
+if (isset($_POST['login'])){
+    if (isset($_POST["password"])){
+        
+        $sql="SELECT COUNT(*) FROM admins"; 
+
+        $connexion=dbconnect(); 
+        if(!$connexion->query($sql)) {
+            echo "Pb d'accès à la bdd"; 
+        }
+        else{
+            
+            /* Query Prepare */
+            $sql = "SELECT * FROM admins WHERE login = :login AND password=:password";
+            $query = $connexion->prepare($sql);
+            $query->bindValue(':login', $_POST['login'], PDO::PARAM_STR);
+            $query->bindValue(':password', $_POST['password'], PDO::PARAM_STR);
+            $query->execute();
+            $members_array = $query->fetchAll();
+
+            $row_count = count($members_array);
+
+            // Check the number of rows that match the SELECT statement 
+            if($row_count==1) 
+            {
+                $member_row = $members_array[0];
+                $_SESSION['login'] = $member_row['login'];
+            }
+        }
+        $connexion=null;
+    }
+}
