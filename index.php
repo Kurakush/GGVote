89528591d1e030972057b7c5ead8628c639a622d
DEGRELLE Thomas <thomas.degrelle2@etu.univ-lorraine.fr>
2025-11-18 11:09:41 +0100

    tentative de faire un carousel

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php 
-
 require('header.php');
-
 ?>
+
+<div class="main">
+    <div class="slider-container">
+        <div class="carousel">
+            <label for="slide-dot-1"></label>
+            <label for="slide-dot-2"></label>
+            <label for="slide-dot-3"></label>
+        </div>
+    </div>
+
+
