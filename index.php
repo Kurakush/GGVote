7a4a0477f7bd3194eaf7dc1fb6a9ce8470468626
DEGRELLE Thomas <thomas.degrelle2@etu.univ-lorraine.fr>
2025-11-21 09:10:42 +0100

    ajout d'images et sections jeux

--- index.php
+++ index.php
@@ -29,6 +29,21 @@ require('header.php');
         visiteur ou électeur, GGVote vous offre une expérience simple et intuitive.
         </p>
     </div>
+
+    <div class="sections-jeux">
+        <h1>Jeux disponibles</h1>
+        <div class="tuiles">
+            <img src="images/valorant.jpg" alt="Valorant">
+            <h3>Valorant</h3>
+        </div>
+        <div class="tuiles">
+            <img src="images/lol.png" alt="League of Legends">
+            <h3>League of Legends</h3>
+        </div>
+
+
+        </div>
+    </div>
 </div>
 
 
