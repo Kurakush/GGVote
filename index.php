be19bcedd64a840c898cb8b2d864ade1e7e0971e
DEGRELLE Thomas <thomas.degrelle2@etu.univ-lorraine.fr>
2025-11-21 08:46:07 +0100

    ecriture d'une partie du site

--- index.php
+++ index.php
@@ -16,7 +16,22 @@ require('header.php');
         <input id="slide-dot-3" type="radio" name="slides">
         <div class="slide slide-3"></div>
     </div>
+
+    <div class="about-us">
+        <h1>A propos de GGVote</h1>
+        <p>GGVote est une plateforme de vote en ligne dédiée aux compétitions e-sport.
+            Elle permet aux fans de voter pour les meilleurs joueurs de chaque compétition majeure
+            dans différents jeux tels que League of Legends, Valorant, CSGO:2 et bien d'autres.
+        </p>
+
+        <p>Notre système garantit un vote <strong>unique</strong>, <strong>anonyme</strong> et
+        <strong>sécurisé</strong> afin d'assurer un classement transparent et fiable. Que vous soyez
+        visiteur ou électeur, GGVote vous offre une expérience simple et intuitive.
+        </p>
+    </div>
 </div>
+
+
 
 <?php
 require('footer.php');
