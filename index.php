8257d97b5b269c68e2c0499b4b8b89221b08f2a2
DEGRELLE Thomas <thomas.degrelle2@etu.univ-lorraine.fr>
2025-11-21 09:18:56 +0100

    ajout images + styles des tuiles

--- index.php
+++ index.php
@@ -30,20 +30,29 @@ require('header.php');
         </p>
     </div>
 
+    <h1>Jeux disponibles</h1>
     <div class="sections-jeux">
-        <h1>Jeux disponibles</h1>
         <div class="tuiles">
             <img src="images/valorant.jpg" alt="Valorant">
             <h3>Valorant</h3>
         </div>
+
         <div class="tuiles">
             <img src="images/lol.png" alt="League of Legends">
             <h3>League of Legends</h3>
         </div>
 
+        <div class="tuiles">
+            <img src="images/csgo.jpg" alt="CSGO">
+            <h3>CSGO:2</h3>
+        </div>
 
+        <div class="tuiles">
+            <img src="images/fortnite.png" alt="Fortnite">
+            <h3>Fortnite</h3>
         </div>
     </div>
+    
 </div>
 
 
