8ed09f2d94fb605c1c71b0c516ccdda07236846a
DEGRELLE Thomas <thomas.degrelle2@etu.univ-lorraine.fr>
2025-11-21 13:08:50 +0100

    changement footer

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <!-- Footer -->
     <footer class="text-center">
-    <p>GGVote Copyright - 2024. Tous droits réservés.</p>
+    <p>GGVote Copyright - 2025. Tous droits réservés.</p>
     <p>Projet réalisé dans le cadre du BUT INFORMATIQUE.</p>
     <p>DEGRELLE Thomas et LACROIX Eve</p>
     </footer>
