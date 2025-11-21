7ad1126a19db2e2bb35466be76e482c7daac9837
Eve <eve.lacroix6@etu.univ-lorraine.fr>
2025-11-21 11:46:51 +0100

    ajout de la présentation

--- voter_rl.php
+++ voter_rl.php
@@ -1,3 +1,11 @@
 <?php 
 require('header.php');
 ?>
+
+<div class="about-us">
+        <img src="images/rocketleague.jpg" alt="RL" width=200px height= 200px>
+        <h1>MVP Rocket League</h1>
+        <p>Voter pour élir le MVP de Rocket League par compétitions !</p>
+          <p> <strong> Attention votre vote ne sera plus modifiable après la validation</strong>
+        </p>
+    </div>
