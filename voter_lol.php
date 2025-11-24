1dc26c6f2e68ab7541dfd17d0a6e40696ad14371
Eve <eve.lacroix6@etu.univ-lorraine.fr>
2025-11-24 12:06:56 +0100

    ajout des cartes joueurs

--- voter_lol.php
+++ voter_lol.php
@@ -1,3 +1,183 @@
 <?php 
 require('header.php');
 ?>
+
+<div class="about-us">
+        <img src="images/valorant.jpg" alt="Valorant" width=200px height= 200px>
+        <h1>MVP Valorant</h1>
+        <p>Voter pour élir le MVP de Valorant par compétitions !</p>
+          <p> <strong> Attention votre vote ne sera plus modifiable après la validation</strong>
+        </p>
+</div>
+
+<div class="compet">
+        <h2>Champions Valorant</h2>
+</div>
+<div class="sections-jeux">
+        <div class="tuiles">
+            <img src="images/aspas.webp" alt="aspas">
+            <h3>Aspas</h3>
+            <p> Joueur pro chez MIBR </p>
+        </div>
+
+        <div class="tuiles">
+            <img src="images/brawk.webp" alt="brawk">
+            <h3>Brawk</h3>
+            <p> Joueur pro chez NRG </p>
+        </div>
+
+        <div class="tuiles">
+            <img src="images/kaajak.jpg" alt="kaajak">
+            <h3>Kaajak</h3>
+            <p> Joueur pro chez FNATIC </p>
+        </div>
+
+        <div class="tuiles">
+            <img src="images/cortezia.jpg" alt="cortezia">
+            <h3>Cortezia</h3>
+            <p> Joueur pro chez MIBR </p>
+        </div>
+</div>
+<div class="sections-jeux">
+
+        <div class="tuiles">
+            <img src="images/noman.jpg" alt="noman">
+            <h3>NoMan</h3>
+            <p> Joueur pro chez XLG </p>
+        </div>
+
+        <div class="tuiles">
+            <img src="images/riens.jpg" alt="riens">
+            <h3>RieNs</h3>
+            <p> Joueur pro chez HERETICS </p>
+        </div>
+    
+
+    <div class="tuiles">
+            <img src="images/hyunmin.jpg" alt="hyunmin">
+            <h3>HYUNMIN</h3>
+            <p> Joueur pro chez DRX </p>
+        </div>
+
+    <div class="tuiles">
+            <img src="images/jawgemo.jpg" alt="jawgemo">
+            <h3>Jawgemo</h3>
+            <p> Joueur pro chez G2 </p>
+        </div>
+</div>
+    
+</div>
+
+<div class="compet">
+        <h2>VCT EMEA</h2>
+</div>
+
+<div class="sections-jeux">
+        <div class="tuiles">
+            <img src="images/derke.jpg" alt="derke">
+            <h3>Derke</h3>
+            <p> Joueur pro chez Team Vitality </p>
+        </div>
+
+        <div class="tuiles">
+            <img src="images/miniboo.jpg" alt="miniboo">
+            <h3>Miniboo</h3>
+            <p> Joueur pro chez Team Heretics </p>
+        </div>
+
+        <div class="tuiles">
+            <img src="images/koalanoob.jpg" alt="koalanoob">
+            <h3>Koalanoob</h3>
+            <p> Joueur pro chez Natus Vincere </p>
+        </div>
+
+        <div class="tuiles">
+            <img src="images/xeus.jpg" alt="xeus">
+            <h3>Xeus</h3>
+            <p> Joueur pro chez FUT Esports </p>
+        </div>
+</div>
+<div class="sections-jeux">
+
+        <div class="tuiles">
+            <img src="images/kaajak.jpg" alt="kaajak">
+            <h3>Kaajak</h3>
+            <p> Joueur pro chez FNATIC </p>
+        </div>
+
+        <div class="tuiles">
+            <img src="images/riens.jpg" alt="riens">
+            <h3>RieNs</h3>
+            <p> Joueur pro chez HERETICS </p>
+        </div>
+    
+
+    <div class="tuiles">
+            <img src="images/alfajer.jpg" alt="alfajer">
+            <h3>Alfajer</h3>
+            <p> Joueur pro chez FNATIC </p>
+        </div>
+
+    <div class="tuiles">
+            <img src="images/boo.jpg" alt="boo">
+            <h3>Boo</h3>
+            <p> Joueur pro chez Heretics </p>
+        </div>
+</div>
+
+<div class="compet">
+        <h2>VCT PACIFIC</h2>
+</div>
+
+<div class="sections-jeux">
+        <div class="tuiles">
+            <img src="images/meiy.jpg" alt="meiy">
+            <h3>Meiy</h3>
+            <p> Joueur pro chez DFM </p>
+        </div>
+
+        <div class="tuiles">
+            <img src="images/hyunmin.jpg" alt="hyunmin">
+            <h3>HYUNMIN</h3>
+            <p> Joueur pro chez DRX </p>
+        </div>
+
+        <div class="tuiles">
+            <img src="images/jemkin.jpg" alt="jemkin">
+            <h3>Jemkin</h3>
+            <p> Joueur pro chez RRQ </p>
+        </div>
+
+        <div class="tuiles">
+            <img src="images/karon.jpg" alt="karon">
+            <h3>Karon</h3>
+            <p> Joueur pro chez GEN.G </p>
+        </div>
+</div>
+<div class="sections-jeux">
+
+        <div class="tuiles">
+            <img src="images/something.jpg" alt="something">
+            <h3>Something</h3>
+            <p> Joueur pro chez Paper Rex </p>
+        </div>
+
+        <div class="tuiles">
+            <img src="images/d4v41.jpg" alt="d4v41">
+            <h3>D4v41</h3>
+            <p> Joueur pro chez Paper Rex </p>
+        </div>
+    
+
+    <div class="tuiles">
+            <img src="images/meteor.jpg" alt="meteor">
+            <h3>Meteor</h3>
+            <p> Joueur pro chez T1 </p>
+        </div>
+
+    <div class="tuiles">
+            <img src="images/maKo.jpg" alt="mako">
+            <h3>MaKo</h3>
+            <p> Joueur pro chez DRX </p>
+        </div>
+</div>
