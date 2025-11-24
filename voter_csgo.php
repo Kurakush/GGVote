d4d3a21dcf37c639d6a0c779043156b74182e9a0
DEGRELLE Thomas <thomas.degrelle2@etu.univ-lorraine.fr>
2025-11-24 12:07:55 +0100

    ajout des joueurs CSGO

--- voter_csgo.php
+++ voter_csgo.php
@@ -3,66 +3,66 @@ require('header.php');
 ?>
 
 <div class="about-us">
-        <img src="images/valorant.jpg" alt="Valorant" width=200px height= 200px>
-        <h1>MVP Valorant</h1>
-        <p>Voter pour élir le MVP de Valorant par compétitions !</p>
+        <img src="images/csgo.jpg" alt="CSGO:2" width=300px height= 200px>
+        <h1>MVP CSGO:2</h1>
+        <p>Voter pour élir le MVP de CSGO:2 par compétitions !</p>
           <p> <strong> Attention votre vote ne sera plus modifiable après la validation</strong>
         </p>
 </div>
 
 <div class="compet">
-        <h2>Champions Valorant</h2>
+        <h2>PGL Major Copenhagen</h2>
 </div>
 <div class="sections-jeux">
         <div class="tuiles">
-            <img src="images/aspas.webp" alt="aspas">
-            <h3>Aspas</h3>
-            <p> Joueur pro chez MIBR </p>
+            <img src="images/jL.jpg" alt="jL">
+            <h3>jL</h3>
+            <p> Joueur pro chez NAVI </p>
         </div>
 
         <div class="tuiles">
-            <img src="images/brawk.webp" alt="brawk">
-            <h3>Brawk</h3>
-            <p> Joueur pro chez NRG </p>
+            <img src="images/monesy.jpg" alt="monesy">
+            <h3>mONESY</h3>
+            <p> Joueur pro chez Falcons </p>
         </div>
 
         <div class="tuiles">
-            <img src="images/kaajak.jpg" alt="kaajak">
-            <h3>Kaajak</h3>
-            <p> Joueur pro chez FNATIC </p>
+            <img src="images/donk.jpg" alt="donk">
+            <h3>donk</h3>
+            <p> Joueur pro chez Team Spirit </p>
         </div>
 
         <div class="tuiles">
-            <img src="images/cortezia.jpg" alt="cortezia">
-            <h3>Cortezia</h3>
-            <p> Joueur pro chez MIBR </p>
+            <img src="images/niko.avif" alt="niko">
+            <h3>NiKo</h3>
+            <p> Joueur pro chez G2 </p>
         </div>
 </div>
 <div class="sections-jeux">
 
         <div class="tuiles">
-            <img src="images/noman.jpg" alt="noman">
-            <h3>NoMan</h3>
-            <p> Joueur pro chez XLG </p>
+            <img src="images/zywoo.jpg" alt="zywoo">
+            <h3>ZywOo</h3>
+            <p> Joueur pro chez Vitality </p>
         </div>
 
         <div class="tuiles">
-            <img src="images/riens.jpg" alt="riens">
-            <h3>RieNs</h3>
-            <p> Joueur pro chez HERETICS </p>
+            <img src="images/broky.jpg" alt="broky">
+            <h3>broky</h3>
+            <p> Joueur pro chez FaZe Clan </p>
         </div>
     
 
     <div class="tuiles">
-            <img src="images/hyunmin.jpg" alt="hyunmin">
-            <h3>HYUNMIN</h3>
-            <p> Joueur pro chez DRX </p>
+            <img src="images/bit.jpg" alt="bit">
+            <h3>b1t</h3>
+            <p> Joueur pro chez NAVI </p>
         </div>
 
     <div class="tuiles">
-            <img src="images/jawgemo.jpg" alt="jawgemo">
-            <h3>Jawgemo</h3>
-            <p> Joueur pro chez G2 </p>
+            <img src="images/frozen.jpg" alt="frozen">
+            <h3>frozen</h3>
+            <p> Joueur pro chez FaZe Clan </p>
         </div>
 </div>
     
