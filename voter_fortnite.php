2139ec91506aeeb3b8f6aa19e991d6596c53bd20
DEGRELLE Thomas <thomas.degrelle2@etu.univ-lorraine.fr>
2025-11-25 15:25:13 +0100

    début de la section fortnite

--- voter_fortnite.php
+++ voter_fortnite.php
@@ -1,3 +1,187 @@
 <?php 
 require('header.php');
 ?>
+
+<div class="about-us">
+        <img src="images/fortnite1.png" alt="fortnite" width=300px height= 200px>
+        <h1>MVP CSGO:2</h1>
+        <p>Voter pour élir le MVP de CSGO:2 par compétitions !</p>
+          <p> <strong> Attention votre vote ne sera plus modifiable après la validation</strong>
+        </p>
+</div>
+
+<div class="compet">
+        <h2>PGL Major Copenhagen 2024</h2>
+</div>
+<div class="sections-jeux">
+        <div class="tuiles">
+            <img src="images/jL.jpg" alt="jL">
+            <h3>jL</h3>
+            <p> Joueur chez NAVI </p>
+        </div>
+
+        <div class="tuiles">
+            <img src="images/monesy.jpg" alt="monesy">
+            <h3>mONESY</h3>
+            <p> Joueur chez FALCONS </p>
+        </div>
+
+        <div class="tuiles">
+            <img src="images/donk.jpg" alt="donk">
+            <h3>donk</h3>
+            <p> Joueur chez TEAM SPIRIT </p>
+        </div>
+
+        <div class="tuiles">
+            <img src="images/niko.avif" alt="niko">
+            <h3>NiKo</h3>
+            <p> Joueur chez G2 </p>
+        </div>
+</div>
+<div class="sections-jeux">
+
+        <div class="tuiles">
+            <img src="images/zywoo.jpg" alt="zywoo">
+            <h3>ZywOo</h3>
+            <p> Joueur chez VITALITY </p>
+        </div>
+
+        <div class="tuiles">
+            <img src="images/broky.jpg" alt="broky">
+            <h3>broky</h3>
+            <p> Joueur chez FAZE CLAN </p>
+        </div>
+    
+
+    <div class="tuiles">
+            <img src="images/bit.jpg" alt="bit">
+            <h3>b1t</h3>
+            <p> Joueur chez NAVI </p>
+        </div>
+
+    <div class="tuiles">
+            <img src="images/frozen.jpg" alt="frozen">
+            <h3>frozen</h3>
+            <p> Joueur chez FAZE CLAN </p>
+        </div>
+</div>
+    
+</div>
+
+<div class="compet">
+        <h2>BLAST.tv Austin Major 2025</h2>
+</div>
+
+<div class="sections-jeux">
+        <div class="tuiles">
+            <img src="images/zywoo.jpg" alt="zywoo">
+            <h3>Zywoo</h3>
+            <p> Joueur chez VITALITY </p>
+        </div>
+
+        <div class="tuiles">
+            <img src="images/donk.jpg" alt="donk">
+            <h3>Donk</h3>
+            <p> Joueur chez TEAM SPIRIT </p>
+        </div>
+
+        <div class="tuiles">
+            <img src="images/senzu.avif" alt="senzu">
+            <h3>Senzu</h3>
+            <p> Joueur chez THE MONGOLZ </p>
+        </div>
+
+        <div class="tuiles">
+            <img src="images/ropz.jpg" alt="ropz">
+            <h3>Ropz</h3>
+            <p> Joueur chez VITALITY </p>
+        </div>
+</div>
+<div class="sections-jeux">
+
+        <div class="tuiles">
+            <img src="images/mezi.jpg" alt="mezi">
+            <h3>Mezii</h3>
+            <p> Joueur chez VITALITY </p>
+        </div>
+
+        <div class="tuiles">
+            <img src="images/910.jpg" alt="910">
+            <h3>910</h3>
+            <p> Joueur chez THE MONGOLZ </p>
+        </div>
+    
+
+    <div class="tuiles">
+            <img src="images/nqz.jpg" alt="nqz">
+            <h3>nqz</h3>
+            <p> Joueur chez PAIN GAMING </p>
+        </div>
+
+    <div class="tuiles">
+            <img src="images/simple.jpg" alt="simple">
+            <h3>s1mple</h3>
+            <p> Joueur chez FAZE CLAN </p>
+        </div>
+</div>
+
+<div class="compet">
+        <h2>Perfect World Shanghai Major 2024</h2>
+</div>
+
+<div class="sections-jeux">
+        <div class="tuiles">
+            <img src="images/donk.jpg" alt="donk">
+            <h3>Donk</h3>
+            <p> Joueur chez TEAM SPIRIT </p>
+        </div>
+
+        <div class="tuiles">
+            <img src="images/zywoo.jpg" alt="zywoo">
+            <h3>Zywoo</h3>
+            <p> Joueur chez VITALITY </p>
+        </div>
+
+        <div class="tuiles">
+            <img src="images/niko.avif" alt="niko">
+            <h3>Niko</h3>
+            <p> Joueur chez G2 </p>
+        </div>
+
+        <div class="tuiles">
+            <img src="images/monesy.jpg" alt="monesy">
+            <h3>Monesy</h3>
+            <p> Joueur chez G2 </p>
+        </div>
+</div>
+<div class="sections-jeux">
+
+        <div class="tuiles">
+            <img src="images/icy.jpg" alt="icy">
+            <h3>ICY</h3>
+            <p> Joueur chez CLOUD9 </p>
+        </div>
+
+        <div class="tuiles">
+            <img src="images/fame.jpg" alt="fame">
+            <h3>fame</h3>
+            <p> Joueur chez VIRTUS.PRO </p>
+        </div>
+    
+
+    <div class="tuiles">
+            <img src="images/flamez.jpg" alt="flamez">
+            <h3>flameZ</h3>
+            <p> Joueur chez VITALITY </p>
+        </div>
+
+    <div class="tuiles">
+            <img src="images/spinx.jpg" alt="spinx">
+            <h3>Spinx</h3>
+            <p> Joueur chez VITALITY </p>
+        </div>
+</div>
+
+<?php
+require('footer.php');
+?>
