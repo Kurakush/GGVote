66affb84298675fad9e39ec63348eaef3f0ef0cb
Eve <eve.lacroix6@etu.univ-lorraine.fr>
2025-11-27 10:01:51 +0100

    modif taille logo

--- voter_rl.php
+++ voter_rl.php
@@ -3,7 +3,7 @@ require('header.php');
 ?>
 
 <div class="about-us">
-        <img src="images/rocketleague.jpg" alt="RL" width=200px height= 200px>
+        <img src="images/rocketleague.jpg" alt="RL" width=384px height= 216px>
         <h1>MVP Rocket League</h1>
         <p>Voter pour élir le MVP de Rocket League par compétitions !</p>
           <p> <strong> Attention votre vote ne sera plus modifiable après la validation</strong>
