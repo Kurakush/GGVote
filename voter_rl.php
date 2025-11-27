346b678fa767038e636fb5cdfb7f269be3ee66f4
Eve <eve.lacroix6@etu.univ-lorraine.fr>
2025-11-27 10:00:00 +0100

    modif de la première compétition

--- voter_rl.php
+++ voter_rl.php
@@ -9,3 +9,179 @@ require('header.php');
           <p> <strong> Attention votre vote ne sera plus modifiable après la validation</strong>
         </p>
     </div>
+
+<div class="compet">
+        <h2>Rocket League Championship Series</h2>
+</div>
+<div class="sections-jeux">
+        <div class="tuiles">
+            <img src="images/zen.jpg" alt="zen">
+            <h3>zen</h3>
+            <p> Joueur chez team VITALITY </p>
+        </div>
+
+        <div class="tuiles">
+            <img src="images/firstkiller.jpg" alt="firstkiller">
+            <h3>firstkiller</h3>
+            <p> Joueur chez COMPLEXITY </p>
+        </div>
+
+        <div class="tuiles">
+            <img src="images/jknaps.jpg" alt="jknaps">
+            <h3>jknaps</h3>
+            <p> Joueur chez G2 </p>
+        </div>
+
+        <div class="tuiles">
+            <img src="images/monkeymoon.jpg" alt="monkeymoon">
+            <h3>monkeymoon</h3>
+            <p> Joueur chez BDS </p>
+        </div>
+</div>
+<div class="sections-jeux">
+
+        <div class="tuiles">
+            <img src="images/rise.jpg" alt="rise">
+            <h3>rise</h3>
+            <p> Joueur chez BDS </p>
+        </div>
+
+        <div class="tuiles">
+            <img src="images/vatira.jpg" alt="vatira">
+            <h3>vatira</h3>
+            <p> Joueur chez KC </p>
+        </div>
+    
+
+    <div class="tuiles">
+            <img src="images/atomic.jpg" alt="atomic">
+            <h3>atomic</h3>
+            <p> Joueur chez G2 </p>
+        </div>
+
+    <div class="tuiles">
+            <img src="images/seikoo.jpg" alt="seikoo">
+            <h3>seikoo</h3>
+            <p> Joueur chez BDS </p>
+        </div>
+</div>
+    
+</div>
+
+<div class="compet">
+        <h2>VCT EMEA</h2>
+</div>
+
+<div class="sections-jeux">
+        <div class="tuiles">
+            <img src="images/derke.jpg" alt="derke">
+            <h3>Derke</h3>
+            <p> Joueur chez VITALITY </p>
+        </div>
+
+        <div class="tuiles">
+            <img src="images/miniboo.jpg" alt="miniboo">
+            <h3>Miniboo</h3>
+            <p> Joueur chez HERETICS </p>
+        </div>
+
+        <div class="tuiles">
+            <img src="images/koalanoob.jpg" alt="koalanoob">
+            <h3>Koalanoob</h3>
+            <p> Joueur chez NAVI </p>
+        </div>
+
+        <div class="tuiles">
+            <img src="images/xeus.jpg" alt="xeus">
+            <h3>Xeus</h3>
+            <p> Joueur chez FUT ESPORTS </p>
+        </div>
+</div>
+<div class="sections-jeux">
+
+        <div class="tuiles">
+            <img src="images/kaajak.jpg" alt="kaajak">
+            <h3>Kaajak</h3>
+            <p> Joueur chez FNATIC </p>
+        </div>
+
+        <div class="tuiles">
+            <img src="images/riens.jpg" alt="riens">
+            <h3>RieNs</h3>
+            <p> Joueur chez HERETICS </p>
+        </div>
+    
+
+    <div class="tuiles">
+            <img src="images/alfajer.jpg" alt="alfajer">
+            <h3>Alfajer</h3>
+            <p> Joueur chez FNATIC </p>
+        </div>
+
+    <div class="tuiles">
+            <img src="images/boo.jpg" alt="boo">
+            <h3>Boo</h3>
+            <p> Joueur chez HERETICS </p>
+        </div>
+</div>
+
+<div class="compet">
+        <h2>VCT PACIFIC</h2>
+</div>
+
+<div class="sections-jeux">
+        <div class="tuiles">
+            <img src="images/meiy.jpg" alt="meiy">
+            <h3>Meiy</h3>
+            <p> Joueur chez DFM </p>
+        </div>
+
+        <div class="tuiles">
+            <img src="images/hyunmin.jpg" alt="hyunmin">
+            <h3>HYUNMIN</h3>
+            <p> Joueur chez DRX </p>
+        </div>
+
+        <div class="tuiles">
+            <img src="images/jemkin.jpg" alt="jemkin">
+            <h3>Jemkin</h3>
+            <p> Joueur chez RRQ </p>
+        </div>
+
+        <div class="tuiles">
+            <img src="images/karon.jpg" alt="karon">
+            <h3>Karon</h3>
+            <p> Joueur chez GEN.G </p>
+        </div>
+</div>
+<div class="sections-jeux">
+
+        <div class="tuiles">
+            <img src="images/something.jpg" alt="something">
+            <h3>Something</h3>
+            <p> Joueur chez PAPER REX </p>
+        </div>
+
+        <div class="tuiles">
+            <img src="images/d4v41.jpg" alt="d4v41">
+            <h3>D4v41</h3>
+            <p> Joueur chez PAPER REX </p>
+        </div>
+    
+
+    <div class="tuiles">
+            <img src="images/meteor.jpg" alt="meteor">
+            <h3>Meteor</h3>
+            <p> Joueur chez T1 </p>
+        </div>
+
+    <div class="tuiles">
+            <img src="images/maKo.jpg" alt="mako">
+            <h3>MaKo</h3>
+            <p> Joueur chez DRX </p>
+        </div>
+</div>
+
+<?php
+require('footer.php');
+?>
