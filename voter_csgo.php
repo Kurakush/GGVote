393419d38e41dea26aa6d2e76c6dcf8a4a0e905a
DEGRELLE Thomas <thomas.degrelle2@etu.univ-lorraine.fr>
2025-12-08 11:27:39 +0100

    dynamisme des tuiles

--- voter_csgo.php
+++ voter_csgo.php
@@ -1,188 +1,64 @@
 <?php 
 require('header.php');
+
+$connexion = dbconnect();
+if (!$connexion) {
+    die("Pb d'accès à la bdd");
+}
+
+// 1) Récupérer les compétitions CS2
+$sql = "SELECT idcompetition, nom_compet
+        FROM competition
+        WHERE idjeu = 5";
+$stmt = $connexion->query($sql);
+$competitions = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
 <div class="about-us">
-        <img src="images/csgo.jpg" alt="CSGO:2" width=300px height= 200px>
-        <h1>MVP CSGO:2</h1>
-        <p>Voter pour élir le MVP de CSGO:2 par compétitions !</p>
-          <p> <strong> Attention votre vote ne sera plus modifiable après la validation</strong>
+    <img src="images/csgo.jpg" alt="CS2" width="300" height="200">
+    <h1>MVP CSGO:2</h1>
+    <p>Voter pour élire le MVP de CSGO:2 par compétitions !</p>
+    <p><strong>Attention, votre vote ne sera plus modifiable après la validation.</strong></p>
+</div>
+
+<?php
+// 2) Pour chaque compétition CS2, on affiche les joueurs associés
+foreach ($competitions as $comp) {
+
+    // Joueurs de cette compétition
+    $sqlJ = "SELECT *
+             FROM joueur
+             WHERE idcompetition = :idcomp
+             ORDER BY pseudo";
+    $stmtJ = $connexion->prepare($sqlJ);
+    $stmtJ->execute([':idcomp' => $comp['idcompetition']]);
+    $joueurs = $stmtJ->fetchAll(PDO::FETCH_ASSOC);
+    ?>
+
+    <div class="compet">
+        <h2><?= htmlspecialchars($comp['nom_compet']) ?></h2>
+    </div>
+
+    <?php if (count($joueurs) === 0): ?>
+        <p style="text-align:center;margin-bottom:40px;">
+            Aucun joueur enregistré pour cette compétition.
         </p>
-</div>
-
-<div class="compet">
-        <h2>PGL Major Copenhagen 2024</h2>
-</div>
-<div class="sections-jeux">
-        <div class="tuiles">
-            <img src="images/jL.jpg" alt="jL">
-            <h3>jL</h3>
-            <p> Joueur chez NAVI </p>
+    <?php else: ?>
+        <div class="sections-jeux">
+            <?php foreach ($joueurs as $j): ?>
+                <div class="tuiles">
+                    <?php if (!empty($j['photo'])): ?>
+                        <img src="images/<?= htmlspecialchars($j['photo']) ?>"
+                             alt="<?= htmlspecialchars($j['pseudo']) ?>">
+                    <?php endif; ?>
+                    <h3><?= strtoupper(htmlspecialchars($j['pseudo'])) ?></h3>
+                    <p>Joueur chez <?= htmlspecialchars($j['equipe']) ?></p>
+                </div>
+            <?php endforeach; ?>
         </div>
+    <?php endif; ?>
 
-        <div class="tuiles">
-            <img src="images/monesy.jpg" alt="monesy">
-            <h3>mONESY</h3>
-            <p> Joueur chez FALCONS </p>
-        </div>
-
-        <div class="tuiles">
-            <img src="images/donk.jpg" alt="donk">
-            <h3>donk</h3>
-            <p> Joueur chez TEAM SPIRIT </p>
-        </div>
-
-        <div class="tuiles">
-            <img src="images/niko.avif" alt="niko">
-            <h3>NiKo</h3>
-            <p> Joueur chez G2 </p>
-        </div>
-</div>
-<div class="sections-jeux">
-
-        <div class="tuiles">
-            <img src="images/zywoo.jpg" alt="zywoo">
-            <h3>ZywOo</h3>
-            <p> Joueur chez VITALITY </p>
-        </div>
-
-        <div class="tuiles">
-            <img src="images/broky.jpg" alt="broky">
-            <h3>broky</h3>
-            <p> Joueur chez FAZE CLAN </p>
-        </div>
-    
-
-    <div class="tuiles">
-            <img src="images/bit.jpg" alt="bit">
-            <h3>b1t</h3>
-            <p> Joueur chez NAVI </p>
-        </div>
-
-    <div class="tuiles">
-            <img src="images/frozen.jpg" alt="frozen">
-            <h3>frozen</h3>
-            <p> Joueur chez FAZE CLAN </p>
-        </div>
-</div>
-    
-</div>
-
-<br><br>
-<div class="compet">
-        <h2>BLAST.tv Austin Major 2025</h2>
-</div>
-
-<div class="sections-jeux">
-        <div class="tuiles">
-            <img src="images/zywoo.jpg" alt="zywoo">
-            <h3>Zywoo</h3>
-            <p> Joueur chez VITALITY </p>
-        </div>
-
-        <div class="tuiles">
-            <img src="images/donk.jpg" alt="donk">
-            <h3>Donk</h3>
-            <p> Joueur chez TEAM SPIRIT </p>
-        </div>
-
-        <div class="tuiles">
-            <img src="images/senzu.avif" alt="senzu">
-            <h3>Senzu</h3>
-            <p> Joueur chez THE MONGOLZ </p>
-        </div>
-
-        <div class="tuiles">
-            <img src="images/ropz.jpg" alt="ropz">
-            <h3>Ropz</h3>
-            <p> Joueur chez VITALITY </p>
-        </div>
-</div>
-<div class="sections-jeux">
-
-        <div class="tuiles">
-            <img src="images/mezi.jpg" alt="mezi">
-            <h3>Mezii</h3>
-            <p> Joueur chez VITALITY </p>
-        </div>
-
-        <div class="tuiles">
-            <img src="images/910.jpg" alt="910">
-            <h3>910</h3>
-            <p> Joueur chez THE MONGOLZ </p>
-        </div>
-    
-
-    <div class="tuiles">
-            <img src="images/nqz.jpg" alt="nqz">
-            <h3>nqz</h3>
-            <p> Joueur chez PAIN GAMING </p>
-        </div>
-
-    <div class="tuiles">
-            <img src="images/simple.jpg" alt="simple">
-            <h3>s1mple</h3>
-            <p> Joueur chez FAZE CLAN </p>
-        </div>
-</div>
-
-<br><br>
-<div class="compet">
-        <h2>Perfect World Shanghai Major 2024</h2>
-</div>
-
-<div class="sections-jeux">
-        <div class="tuiles">
-            <img src="images/donk.jpg" alt="donk">
-            <h3>Donk</h3>
-            <p> Joueur chez TEAM SPIRIT </p>
-        </div>
-
-        <div class="tuiles">
-            <img src="images/zywoo.jpg" alt="zywoo">
-            <h3>Zywoo</h3>
-            <p> Joueur chez VITALITY </p>
-        </div>
-
-        <div class="tuiles">
-            <img src="images/niko.avif" alt="niko">
-            <h3>Niko</h3>
-            <p> Joueur chez G2 </p>
-        </div>
-
-        <div class="tuiles">
-            <img src="images/monesy.jpg" alt="monesy">
-            <h3>Monesy</h3>
-            <p> Joueur chez G2 </p>
-        </div>
-</div>
-<div class="sections-jeux">
-
-        <div class="tuiles">
-            <img src="images/icy.jpg" alt="icy">
-            <h3>ICY</h3>
-            <p> Joueur chez CLOUD9 </p>
-        </div>
-
-        <div class="tuiles">
-            <img src="images/fame.jpg" alt="fame">
-            <h3>fame</h3>
-            <p> Joueur chez VIRTUS.PRO </p>
-        </div>
-    
-
-    <div class="tuiles">
-            <img src="images/flamez.jpg" alt="flamez">
-            <h3>flameZ</h3>
-            <p> Joueur chez VITALITY </p>
-        </div>
-
-    <div class="tuiles">
-            <img src="images/spinx.jpg" alt="spinx">
-            <h3>Spinx</h3>
-            <p> Joueur chez VITALITY </p>
-        </div>
-</div>
+<?php } // fin foreach compétitions ?>
 
 <?php
 require('footer.php');
